9d66c6156aceb304d11a95f2485757a928834ee2
milo19901101 <liuyizhen19901101@163.com>
2019-10-21 14:09:38 +0800

    swagger

--- src/main/java/com/jiaxin/pda/conponent/SwaggerConfig.java
+++ src/main/java/com/jiaxin/pda/conponent/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.jiaxin.pda.conponent;
 
 
 import com.jiaxin.pda.constant.Constant;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -12,17 +13,23 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import javax.validation.Valid;
+
 /**
  * swagger配置文件
  * @author milo
  */
 @Configuration
 public class SwaggerConfig {
-    
+
+    @Value("${swagger.enable}")
+    private Boolean enable = true;
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .enable(enable)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(Constant.BASE_PACKAGE))
                 .paths(PathSelectors.any())
