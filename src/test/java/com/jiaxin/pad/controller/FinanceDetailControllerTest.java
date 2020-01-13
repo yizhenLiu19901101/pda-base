f284edf59412132832535075fca7425ef35139ae
milo19901101 <liuyizhen19901101@163.com>
2020-01-13 16:08:49 +0800

    BUG修复

--- src/test/java/com/jiaxin/pad/controller/FinanceDetailControllerTest.java
+++ src/test/java/com/jiaxin/pad/controller/FinanceDetailControllerTest.java
@@ -128,8 +128,8 @@ public class FinanceDetailControllerTest extends AbstractTestNGSpringContextTest
     public void queryByToken() throws Exception{
         logger.info("查询财务信息");
         //执行测试
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/finance/getDetailDate/1")
-                .header("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1Nzg5ODk4NDM0MDYsInBheWxvYWQiOiJcIjI1Yjk1NzkwOGExZDQ2MTI4MDdlMGE4ZjZmMGU4OGU1XCIifQ.LcRZ2RYmlp04NaZfA0ZgYcS16__AkHeu5QvMsz0lll8")
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/finance/getDetailDate/2")
+                .header("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1Nzk1MDc1NzQ0MjIsInBheWxvYWQiOiJcIjI1Yjk1NzkwOGExZDQ2MTI4MDdlMGE4ZjZmMGU4OGU1XCIifQ.GFCupog4i4pTBTFA8spukxUiXRniCUdFM6hPgdNuRhc")
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk());
