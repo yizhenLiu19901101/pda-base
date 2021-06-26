1d958df58656ae3d5e7e8b372ec898108510a9c5
liuyizhen <2124459055@qq.com>
2021-06-26 14:20:56 +0800

    代码调整

--- src/main/java/com/jiaxin/pda/PdaApplication.java
+++ src/main/java/com/jiaxin/pda/PdaApplication.java
@@ -4,9 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -14,8 +11,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @author milo
  */
 @SpringBootApplication
-@EnableEurekaClient   // eureka客户端
-@EnableFeignClients   // feign客户端
 @EnableSwagger2
 public class PdaApplication {
     private static final Logger logger = LoggerFactory.getLogger(PdaApplication.class);
---
+++ src/main/java/com/jiaxin/pda/config/CrosConfig.java
@@ -0,0 +1,22 @@
+package com.jiaxin.pda.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 跨域问题解决
+ */
+@Configuration
+class CrosConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("----------------------");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+    }
+}
